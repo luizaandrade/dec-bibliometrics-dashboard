8dd4a68fe39604017e138b1a11933079e5aeedf2
LeonardoViotti <leonardoviotti@gmail.com>
2021-11-09 08:39:24 -0500

    Scrapping class template.

---
+++ scraping/crawler.py
@@ -0,0 +1,13 @@
+
+import requests
+from lxml import html
+from bs4 import BeautifulSoup
+
+class OKRCrawler:
+    """
+    """
+    def __init__(self):
+        pass
+
+if __name__ == "__main__":
+    print("Foo!")
