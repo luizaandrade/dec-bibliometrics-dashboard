293297e83169af99df8d2f174571d606359ff69f
LeonardoViotti <leonardoviotti@gmail.com>
2021-11-09 08:45:31 -0500

    Scrapping psedo-code.

--- scraping/crawler.py
+++ scraping/crawler.py
@@ -6,7 +6,21 @@ from bs4 import BeautifulSoup
 class OKRCrawler:
     """
     """
-    def __init__(self):
+    def __init__(self,
+                 base_url,
+                 handles_df):
+        self.base_url = base_url
+        self.handles_df = handles_df
+    
+    def get_static_html(self):
+        pass
+    def get_stats_json(self):
+        pass
+    def process_json(self):
+        pass
+    def crawl(self):
+        pass
+    def create_df(self):
         pass
 
 if __name__ == "__main__":
