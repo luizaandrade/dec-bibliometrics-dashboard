f2c9933665773716e931550a71f478e4e169171b
LeonardoViotti <leonardoviotti@gmail.com>
2021-11-15 12:04:05 -0500

    Slightly better crawler.

--- scraping/crawler.py
+++ scraping/crawler.py
@@ -24,7 +24,7 @@ class OKRCrawler:
         # Load handles df
         self.df = pd.read_csv(self.handles_df_path)
         
-    def get_static_html(self, handle, export_html = True):
+    def get_static_html(self, handle, export_html = False):
         """
         Loads html for static page
         """
@@ -43,6 +43,8 @@ class OKRCrawler:
             # Whoops it wasn't a 200
             error = "Error: " + str(e)
             print(error)
+            return e
+            # return None
         # else:
             # print(" Successfully loaded static page HTML...")
         finally:
@@ -90,43 +92,51 @@ class OKRCrawler:
         """
         self.get_static_html(handle)
         
-        # Get static info --------------------------------
-        
-        # citation
-        citation =  self.soup.find_all("div", {"class": "citation"})[0].text
-        # remove special characters
-        citation = citation.replace('\n', "").replace('“', '')
-        
-        # Get dsoid param from static html -> class="embed-cua-widget" data-dso-id = (...)
-        embed_html_elem = self.soup.find_all("div", {"class": "embed-cua-widget"})
-        dsoid = embed_html_elem[0]["data-dso-id"]
-        
-        # Get JSON with stats info -----------------------
-        self.json = self.get_stats_json(dsoid)
-        
-        # Process JSON -----------------------------------
-        
-        # First item on json has info on abstract views
-        abstract_views = self.process_json(self.json, 0)
-        
-        # Third item on json has info on n of downloads
-        downloads = self.process_json(self.json, 2)
-        
-        # Download timestamp
-        timestamp = datetime.now().strftime("%m/%d/%Y %H:%M:%S")
-        
-        return handle, citation, dsoid, abstract_views, downloads, timestamp
+        # Only process request if successfull response
+        if self.page.status_code == 200:
+            # Get static info --------------------------------
+            
+            # citation
+            citation =  self.soup.find_all("div", {"class": "citation"})[0].text
+            # remove special characters
+            citation = citation.replace('\n', "").replace('“', '')
+            
+            # Get dsoid param from static html -> class="embed-cua-widget" data-dso-id = (...)
+            embed_html_elem = self.soup.find_all("div", {"class": "embed-cua-widget"})
+            dsoid = embed_html_elem[0]["data-dso-id"]
+            
+            # Get JSON with stats info -----------------------
+            self.json = self.get_stats_json(dsoid)
+            
+            # Process JSON -----------------------------------
+            
+            # First item on json has info on abstract views
+            abstract_views = self.process_json(self.json, 0)
+            
+            # Third item on json has info on n of downloads
+            downloads = self.process_json(self.json, 2)
+            
+            # Download timestamp
+            timestamp = datetime.now().strftime("%m/%d/%Y %H:%M:%S")
+            
+            return handle, citation, dsoid, abstract_views, downloads, timestamp
     
     def crawl_loop(self, 
                    handles_list = None,
                    export_df = True,
                    export_path = None,
                    columns = ['handle', 'citation', 'dsoid', 'abstract_views', 'downloads', 'scraping_date']):
-        
-        if handles_list is None:
-            handles_list = self.df.Handle
+        # Export variables df
+        time = datetime.now().strftime("%m-%d-%Y-%H-%M")
+        filename = 'okr_results-' + time + '.csv'
+        path = os.getcwd()
+        if export_path is not None:
+            path = export_path
+        file_path = os.path.join(path + filename)
         
         # Loop through all handles in df
+        if handles_list is None:
+            handles_list = self.df.Handle
         row_list = []
         idx = 1
         for handle in handles_list:
@@ -134,29 +144,49 @@ class OKRCrawler:
                     
             print('Scraping {}:'.format(handle))
             row = self.crawl(handle)
-            row_list.append(row)
-            idx += 1
+            print(row)
             
-            # Wait between .5 and 2s before sending another request
-            sleep(round(random.uniform(.5, 2),3))
+            # If error export as it is
+            if row is None:
+                self.results_df = pd.DataFrame(row_list, columns=columns)
+                
+                if export_df:
+                    print('Interrupting loop')
+                    print('Saving results df in {}'.format(file_path))
+                    self.results_df.to_csv(file_path, index= False)
+                    
+                break
+            # Otherwise contiue with loop
+            else:
+                row_list.append(row)
+                idx += 1
+                # Wait between .5 and 2s before sending another request
+                sleep(round(random.uniform(.5, 2),3))
         
         # Create a results df
         df = pd.DataFrame(row_list, columns=columns)
         
-        # Export df
         if export_df:
-            today = datetime.now().strftime("%Y%m%d")
-            filename = 'okr_results-' + today + '.csv'
-            path = os.getcwd()
-            if export_path is not None:
-                path = export_path
-            file_path = os.path.join(path + filename)
             print('Saving results df in {}'.format(file_path))
-            df.to_csv(file_path, index= False)
+            self.results_df.to_csv(file_path, index= False)
 
 if __name__ == "__main__":
     crawler = OKRCrawler('C:/Users/wb519128/Downloads/OKR-Data-2014-21.csv')
-    crawler.crawl_loop(handles_list = crawler.df.Handle.iloc[:5])
+    crawler.crawl_loop(handles_list = crawler.df.Handle)
+
+handle = crawler.df.Handle[5]
+crawler.crawl(handle)
+
+crawler.get_static_html(handle)
+
+# RE-write crawl to handle HTML only if 200
+
+# RE-write loop to handle exception
 
 
-
+try:
+    crawler.crawl(handle)
+except:
+    print("Error: do something")
+else:
+    print("Foo")
